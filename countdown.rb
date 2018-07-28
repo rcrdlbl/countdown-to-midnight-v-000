e2456684e70212975d58e7caa7bde79a50cce510
Przemyslaw Dabrowski <bluh@outlook.com>
2018-07-28 02:57:11 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,13 @@ def countdown(number)
   end
   return "HAPPY NEW YEAR!"
 end
+
+def countdown(number)
+  num = number
+  while num > 0
+    puts "#{num} SECOND(S)!"
+    num -= 1
+    sleep 1
+  end
+  return "HAPPY NEW YEAR!"
+end
